4e3fa47b2b2fa7172e25aac6d0f963b5b55df550
kimseil <llloveshin@naver.com>
2018-09-04 23:15:18 +0900

    [seil] PlaceInfoActivity ver1.1
    delete status bar

--- app/src/main/java/com/example/lllov/projectkjh/PlaceInfoActivity.java
+++ app/src/main/java/com/example/lllov/projectkjh/PlaceInfoActivity.java
@@ -3,11 +3,15 @@ package com.example.lllov.projectkjh;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 public class PlaceInfoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //스테이터스바 삭제
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_palce_info);
     }
 }
