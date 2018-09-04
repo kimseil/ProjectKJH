c2cdfc481d0b917aeb4016ad41201ce901e35c3d
kimseil <llloveshin@naver.com>
2018-09-04 23:10:27 +0900

    [seil] Base Setting & Main

--- app/src/main/java/com/example/lllov/projectkjh/MainActivity.java
+++ app/src/main/java/com/example/lllov/projectkjh/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.lllov.projectkjh;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout btnFindTravel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //스테이터스바 삭제
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //툴바
+        ToolBar toolbar = new ToolBar(this);
+        toolbar.setMenu();
+        toolbar.setTitle("대동여지도");
+        toolbar.setToolbar();
+
+        btnFindTravel = findViewById(R.id.btnFindTravel);
+        btnFindTravel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PlaceInfoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/lllov/projectkjh/PlaceInfoActivity.java
+++ app/src/main/java/com/example/lllov/projectkjh/PlaceInfoActivity.java
@@ -8,6 +8,6 @@ public class PlaceInfoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_palceinfo);
+        setContentView(R.layout.activity_palce_info);
     }
 }
