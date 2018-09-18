f57db2a45378990cdd5e797da59f2efee8481a0c
kimseil <llloveshin@naver.com>
2018-09-18 21:06:32 +0900

    [seil] Navigation Drawer

--- app/src/main/java/com/example/lllov/projectkjh/MainActivity.java
+++ app/src/main/java/com/example/lllov/projectkjh/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.lllov.projectkjh;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     LinearLayout btnFindTravel;
 
     @Override
@@ -19,10 +28,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //툴바
-        ToolBar toolbar = new ToolBar(this);
-        toolbar.setMenu();
-        toolbar.setTitle("대동여지도");
-        toolbar.setToolbar();
+        Toolbar toolbar = new ToolBar(this).setTitle("대동여지도").setToolbar();
+
+        //햄버거메뉴
+        DrawerLayout drawer = findViewById(R.id.dl);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nv);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //햄버거메뉴 닫기 버튼
+        ((ImageView)navigationView.getHeaderView(0).findViewById(R.id.btnBack)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
 
         btnFindTravel = findViewById(R.id.btnFindTravel);
         btnFindTravel.setOnClickListener(new View.OnClickListener() {
@@ -32,5 +55,41 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+    }
+
+    //햄버거메뉴가 열려있을때 뒤로가기 버튼 클릭시 메뉴를 닫음
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.dl);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    //햄버거메뉴 각 네비게이션 선택 처리
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        String s = "";
+
+        switch (item.getItemId()) {
+            case R.id.nav_a:
+                s = "A 선택";
+                break;
+            case R.id.nav_b:
+                s = "B 선택";
+                break;
+            default:
+                break;
+        }
+
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+        DrawerLayout drawer = findViewById(R.id.dl);
+        drawer.closeDrawer(GravityCompat.START);
+        return false;
     }
 }
--- app/src/main/java/com/example/lllov/projectkjh/ToolBar.java
+++ app/src/main/java/com/example/lllov/projectkjh/ToolBar.java
@@ -9,47 +9,48 @@ import android.widget.TextView;
 public class ToolBar {
     private AppCompatActivity mActivity;
     private Toolbar mToolbar;
+    private ImageView btnBack;
+    private TextView tvTitle;
 
     //툴바 호출한 액티비티 가져오기
     public ToolBar(AppCompatActivity activity) {
         mActivity = activity;
-    }
 
-    //툴바 생성
-    public void setToolbar() {
         mToolbar = mActivity.findViewById(R.id.toolbar);
-        mActivity.setSupportActionBar(mToolbar);
-    }
 
-    //타이틀
-    public void setTitle(String title) {
-        TextView tvTitle = mActivity.findViewById(R.id.tvTitle);
-        tvTitle.setText(title);
-    }
+        tvTitle = mActivity.findViewById(R.id.tvTitle);
+        btnBack = mActivity.findViewById(R.id.btnBack);
 
-    //뒤로가기 버튼 생성
-    public void setBack() {
-        ImageView btnBack = mActivity.findViewById(R.id.btnBack);
-        btnBack.setClickable(true);
-        btnBack.setVisibility(View.VISIBLE);
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mActivity.onBackPressed();
+                onBtnBack();
             }
         });
     }
 
-    //햄버거 버튼 생성
-    public void setMenu() {
-        ImageView btnMenu = mActivity.findViewById(R.id.btnMenu);
-        btnMenu.setClickable(true);
-        btnMenu.setVisibility(View.VISIBLE);
-        btnMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //햄버거
-            }
-        });
+    //툴바 생성
+    public Toolbar setToolbar() {
+        mActivity.setSupportActionBar(mToolbar);
+
+        return mToolbar;
+    }
+
+    //타이틀
+    public ToolBar setTitle(String title) {
+        tvTitle.setText(title);
+
+        return this;
+    }
+
+    //뒤로가기 버튼
+    public ToolBar setBack() {
+        btnBack.setVisibility(View.VISIBLE);
+
+        return this;
+    }
+
+    private void onBtnBack() {
+        mActivity.onBackPressed();
     }
 }
