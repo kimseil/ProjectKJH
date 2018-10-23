7460a8d6338deb82e2b72b145ade5b61ba9f2573
kimseil <llloveshin@naver.com>
2018-10-23 18:06:31 +0900

    [seil] splash

--- app/src/main/java/com/example/lllov/projectkjh/LoginActivity.java
+++ app/src/main/java/com/example/lllov/projectkjh/LoginActivity.java
@@ -1,13 +1,35 @@
 package com.example.lllov.projectkjh;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends BaseActivity {
+
+    TextView btnLoginGuest;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         deleteStatusBar();
         setContentView(R.layout.activitiy_login);
+
+        btnLoginGuest = findViewById(R.id.btnLoginGuest);
+
+        btnLoginGuest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginGuest();
+            }
+        });
+    }
+
+    private void loginGuest() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+        overridePendingTransition(0, 0);
     }
 }
