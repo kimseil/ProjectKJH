a9db8e05b28a40561907bf5bc4eb8104918b3aa6
kimseil <llloveshin@naver.com>
2018-10-30 16:42:08 +0900

    [seil] Bug Fix

--- app/src/main/java/com/example/lllov/projectkjh/Adapter/AddPlaceAdapter.java
+++ app/src/main/java/com/example/lllov/projectkjh/Adapter/AddPlaceAdapter.java
@@ -1,7 +1,9 @@
 package com.example.lllov.projectkjh.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +14,7 @@ import android.widget.TextView;
 
 import com.example.lllov.projectkjh.BaseActivity;
 import com.example.lllov.projectkjh.DTO.DTOAddPlace;
+import com.example.lllov.projectkjh.PlaceInfoActivity;
 import com.example.lllov.projectkjh.R;
 
 import java.util.ArrayList;
@@ -39,6 +42,15 @@ public class AddPlaceAdapter extends RecyclerView.Adapter<AddPlaceAdapter.ViewHo
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
         viewHolder.tvPlace.setText(data.get(position).getPlace());
         viewHolder.tvLocation.setText(data.get(position).getLocation());
+
+        viewHolder.layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, PlaceInfoActivity.class);
+                context.startActivity(intent);
+                context.overridePendingTransition(R.anim.anim_slide_in_right, R.anim.anim_slide_out_left);
+            }
+        });
     }
 
     @Override
@@ -47,14 +59,16 @@ public class AddPlaceAdapter extends RecyclerView.Adapter<AddPlaceAdapter.ViewHo
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        ConstraintLayout layout;
         ImageView ivPicture;
         TextView tvPlace, tvLocation;
         Button btnSelect;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            layout = itemView.findViewById(R.id.layout);
             ivPicture = itemView.findViewById(R.id.ivPicture);
-            tvPlace = itemView.findViewById(R.id.tvLocation);
+            tvPlace = itemView.findViewById(R.id.tvPlace);
             tvLocation = itemView.findViewById(R.id.tvLocation);
             btnSelect = itemView.findViewById(R.id.btnSelect);
         }
